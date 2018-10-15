53a513d9f21420f167787132c56b8ae60749a117
Satoshi Ogc <hogehogefoobar123@yahoo.co.jp>
2018-10-15 02:13:30 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save # => validation
+      # Success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # GET "/users/#{@user.id}" => show
+    else
+      # Failure
+      render 'new'
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(
+      :name, :email, :password, 
+      :password_confirmation)
   end
 end
